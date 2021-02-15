1ad61a827d3a48f22cfaf796cd0f82d726d05c33
Jorge Guzmán <jorge.guzman@igeckoo.com>
2021-02-15 16:45:18 -0600

    Test 5

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require ('express');
-const index = express();
+const app = express();
 
-index.get('/', (req, res) => {
+app.get('/', (req, res) => {
 	res.send("Hola Mundo");
 });
 
 
 const port = process.env.port || 3000;
 
-index.listen(port, () => {
+app.listen(port, () => {
 	console.log("Hola Mundo");
 });
 
